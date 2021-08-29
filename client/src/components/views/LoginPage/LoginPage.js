6ed09d3d173c4c0bd75020b90524f376f15b7fa7
magarin <mario213@nate.com>
2021-08-30 03:47:41 +0900

    uploadproductpage(1)

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -72,12 +72,10 @@ function LoginPage(props) {
           values,
           touched,
           errors,
-          dirty,
           isSubmitting,
           handleChange,
           handleBlur,
-          handleSubmit,
-          handleReset,
+          handleSubmit
         } = props;
         return (
           <div className="app">
