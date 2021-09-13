5499dc5508345b60520f79bd371fba2ef2cff2aa
magarin <mario213@nate.com>
2021-09-13 16:23:01 +0900

    20210913

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+import {AppleOutlined, }  from '@ant-design/icons';
+import {Card, Row, Col} from "antd"
+import Meta from 'antd/lib/card/Meta';
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
+
+    const renderCards = Products.map((product, index) => {
+        console.log('product',product)
+        return <Col lg={6} md={8} xs={24} key={index}>
+        <Card
+            cover={<img style={{width:"100%", maxHeight:"150px"}} src={`http://localhost:5000/${product.images[0]}`}/>}
+        >
+            <Meta
+            title={product.title}
+            description={`${product.price}`}
+            />
+        </Card>
+        </Col> 
+    })
+
 
     useEffect(() => {
 
@@ -10,6 +30,7 @@ function LandingPage() {
         .then(res => {
             if(res.data.success){
                 console.log(res.data)
+                setProducts(res.data.productInfo)
             } else {
                 alert("상품 조회를 실패 했습니다.")
             }
@@ -17,8 +38,28 @@ function LandingPage() {
     }, [])
 
     return (
-        <div>
-            LandingPage
+        <div style={{width:"75%", margin:"3rem auto"}}>
+            <div style={{textAlign:"center"}}>
+                <h2>Let's Travel Anywhere <AppleOutlined/></h2>
+
+            </div>
+        {/* {Filter} */}
+
+        {/* {Search} */}
+
+        {/* {Cards} */}
+
+            <Row gutter={16, 16}>
+            {renderCards}
+            </Row>
+
+
+
+
+        <div style={{display:"flex", justifyContent:"center"}}>
+            <button>더보기</button>
+        </div>
+
         </div>
     )
 }
